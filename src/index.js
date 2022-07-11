be57af723e68594f5bc61570f2647aecfae69f21
larissaralmeida <larissaralmeida.94@gmail.com>
2022-07-11 16:12:44 -0300

    :construction: Create API with MongoDB

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.listen(3000, 'localhost', () => {
+    console.log('Server online on port: 3000');
+    console.log('Shut down server: ctrl + c');
+});
+
+require('./controllers/authController')(app);
